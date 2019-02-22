d972696fb50d9745c1a0c8aab1ebab85f200332b
Evgeny Bikkinin <e.bikkinin@dodopizza.com>
2019-02-22 10:38:55 +0300

    Initial: add HostBuilder and MediatR dependency

---
+++ MediatRExperiments/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace MediatRExperiments
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureApp()
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
